f62b5d6f41e5f7d2675e7db977e4abeff18dc205
zhouyu131415 <530998147@qq.com>
2018-06-29 18:09:28 +0800

    登陆测试

--- src/test/java/cm/neusoft/shiro/ShiroTest.java
+++ src/test/java/cm/neusoft/shiro/ShiroTest.java
@@ -1,9 +1,11 @@
 package cm.neusoft.shiro;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
-import org.apache.shiro.util.Factory;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 import org.junit.Test;
 
 /*
@@ -22,6 +24,21 @@ public class ShiroTest {
 		//3:将SecurityManager绑定到当前运行环境中，让系统随时都可以访问securityManager对象
 		SecurityUtils.setSecurityManager(securityManager);
 		
-		//4:
+		//4:创建当前登陆的主体
+		Subject subject = SecurityUtils.getSubject();
+		
+		//5:绑定主体登陆的身份/凭证，即账号密码
+		//参数1:将要登陆的用户名，参数2:登陆用户的密码
+		UsernamePasswordToken token = new UsernamePasswordToken("zhangsan","6661");
+		
+		//6:主体登陆
+		subject.login(token);
+		
+		//7:判断登陆是否成功
+		System.out.println("验证登陆是否成功:" + subject.isAuthenticated());
+		
+		//8:登出（注销）
+		subject.logout();
+		System.out.println("验证登陆是否成功:" + subject.isAuthenticated());
 	}
 }
